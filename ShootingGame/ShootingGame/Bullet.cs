8559fc1d86f3518d8fccf1c9a8b1623b79f2c959
Syunsyun1412 <174100719+Syunsyun1412@users.noreply.github.com>
2024-08-28 02:04:36 +0900

    Bulletクラスを作った

---
+++ ShootingGame/ShootingGame/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace ShootingGame
+{
+    public enum Id { PLAYER , ENEMY }
+    public class Bullet :Entity
+    {
+        private int degree;
+        private int damage;
+        private Id id;
+        
+
+        public Bullet(int x, int y, int radius, int speed, int degree, Image img, int damage, Id id) : base(x, y, radius, speed, img)
+        {
+            this.degree = degree;
+            this.damage = damage;
+            this.id = id;
+        }
+        
+         public void Move()
+        {
+            Y -= (int)(Speed * Math.Cos(degree * Math.PI / 180));//degreeは進行方向に対して時計回りに大きくなる。
+            X += (int)(Speed * Math.Sin(degree * Math.PI / 180));
+        }
+    }
+
+}
