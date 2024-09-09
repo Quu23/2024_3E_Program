1347ba23ccd640531dc5858b4f6926024738f079
Quu23 <kiyu@kaz.bbiq.jp>
2024-09-10 00:15:17 +0900

    ImageUriの各定数の名前を変えた。

--- ShootingGame/ShootingGame/Images.cs
+++ ShootingGame/ShootingGame/Images.cs
@@ -7,17 +7,17 @@ namespace ShootingGame
     /// </summary>
     static class Images
     {
-        public static readonly BitmapImage BACKGROUND_IMAGE = new BitmapImage(ImageUris.BACKGROUND);
+        public static readonly BitmapImage BACKGROUND_IMAGE = new BitmapImage(ImageUris.BACKGROUND_URI);
 
-        public static readonly BitmapImage EXP_ORB_IMAGE = new BitmapImage(ImageUris.EXP_ORB);
+        public static readonly BitmapImage EXP_ORB_IMAGE = new BitmapImage(ImageUris.EXP_ORB_URI);
 
-        public static readonly BitmapImage PLAYER_IMAGE = new BitmapImage(ImageUris.PLAYER);
-        public static readonly BitmapImage PLAYER_BULLET_IMAGE = new BitmapImage(ImageUris.P_BULLET);
+        public static readonly BitmapImage PLAYER_IMAGE = new BitmapImage(ImageUris.PLAYER_URI);
+        public static readonly BitmapImage PLAYER_BULLET_IMAGE = new BitmapImage(ImageUris.P_BULLET_URI);
 
-        public static readonly BitmapImage STRAIGHT_ENEMY_IMAGE = new BitmapImage(ImageUris.STRAIGHT_ENEMY);
-        public static readonly BitmapImage SNAKE_ENEMY_IMAGE = new BitmapImage(ImageUris.SNAKE_ENEMY);
-        public static readonly BitmapImage SHOTGUN_ENEMY_IMAGE = new BitmapImage(ImageUris.SHOTGUN_ENEMY);
+        public static readonly BitmapImage STRAIGHT_ENEMY_IMAGE = new BitmapImage(ImageUris.STRAIGHT_ENEMY_URI);
+        public static readonly BitmapImage SNAKE_ENEMY_IMAGE = new BitmapImage(ImageUris.SNAKE_ENEMY_URI);
+        public static readonly BitmapImage SHOTGUN_ENEMY_IMAGE = new BitmapImage(ImageUris.SHOTGUN_ENEMY_URI);
 
-        public static readonly BitmapImage ENEMY_BULLET_IMAGE = new BitmapImage(ImageUris.E_BULLET);
+        public static readonly BitmapImage ENEMY_BULLET_IMAGE = new BitmapImage(ImageUris.E_BULLET_URI);
     }
 }
