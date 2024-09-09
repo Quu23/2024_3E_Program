7d5af4dfe990c3093108616cb4f0d563d237faef
yotsu <sky01star39@gmail.com>
2024-09-10 00:05:32 +0900

    hpを全回復させるアイテムを作りました

---
+++ ShootingGame/ShootingGame/HealingItem.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Media.Imaging;
+
+namespace ShootingGame
+{
+    internal class HealingItem : Item
+    {
+        public HealingItem(int x, int y, BitmapImage img) : base(x, y, 3, 6, null)
+        {
+        }
+
+        public override void MakeEffect(Player player)
+        {
+            player.HeelFullOfHp();
+        }
+
+        public override void CancelEffect(Player player)
+        {
+            ;
+        }
+
+
+        public override void Move()
+        {
+            Y += Speed;
+        }
+    }
+}
