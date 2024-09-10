84e4fa07484ef8471d2637ee49b1291d3d1c59e0
Syunsyun1412 <k.shouta.08.24@gamil.com>
2024-09-10 22:00:02 +0900

    InvincibleItemクラスのコンストラクターとメソッドを表示させた。

--- ShootingGame/ShootingGame/InvincibleItem.cs
+++ ShootingGame/ShootingGame/InvincibleItem.cs
@@ -1,6 +1,26 @@
-﻿namespace ShootingGame
+﻿using System.Windows.Media.Imaging;
+
+namespace ShootingGame
 {
-    internal class InvincibleItem:TransientItem
+    internal class InvincibleItem : TransientItem
     {
+        public InvincibleItem(int x, int y, int radius, int speed, BitmapImage img, int EFFECT_IIME) : base(x, y, radius, speed, img, EFFECT_IIME)
+        {
+        }
+
+        public override void CancelEffect(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MakeEffect(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Move()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
