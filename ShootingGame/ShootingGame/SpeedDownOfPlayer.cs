96e4be8a8b242e754716cf7c99f532c4e6e66423
yotsu <sky01star39@gmail.com>
2024-09-10 22:04:42 +0900

    触れたら一定時間自機のスピードを-3させるデバフアイテムを作りました

---
+++ ShootingGame/ShootingGame/SpeedDownOfPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace ShootingGame
+{
+    internal class SpeedDownOfPlayer : TransientItem
+    {
+        public SpeedDownOfPlayer(int x, int y, BitmapImage img, int EFFECT_IIME) : base(x, y, 4, 6, img, EFFECT_IIME)
+        {
+        }
+
+        public override void CancelEffect(Player player)
+        {
+            player.Speed += 2;
+        }
+
+        public override void MakeEffect(Player player)
+        {
+            player.Speed -= 2;
+        }
+
+        public override void Move()
+        {
+            Y += Speed;
+        }
+    }
+}
