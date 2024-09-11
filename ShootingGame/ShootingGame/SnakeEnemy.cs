cb859afd7b7cbcee4af0a29a60781788a44ae6e3
Quu23 <kiyu@kaz.bbiq.jp>
2024-09-11 21:47:44 +0900

    敵キャラのサイズを変えた。

--- ShootingGame/ShootingGame/SnakeEnemy.cs
+++ ShootingGame/ShootingGame/SnakeEnemy.cs
@@ -4,7 +4,7 @@ namespace ShootingGame
 {
     class SnakeEnemy : Enemy
     {
-        public SnakeEnemy(int x, int y, int level) : base(x, y, /*r=*/8, /*speed=*/5, Images.SNAKE_ENEMY_IMAGE, /*LV=*/level, /*hp=*/3, 50)
+        public SnakeEnemy(int x, int y, int level) : base(x, y, /*r=*/20, /*speed=*/5, Images.SNAKE_ENEMY_IMAGE, /*LV=*/level, /*hp=*/3, 50)
         {
         }
 
