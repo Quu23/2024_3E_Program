80164f9c096579845bae25862ae4f5c0f7926112
Syunsyun1412 <k.shouta.08.24@gamil.com>
2024-09-17 00:11:05 +0900

    GoldenEnemyを作った。

---
+++ ShootingGame/ShootingGame/Entities/Planes/Enemies/GoldenEnemy.cs
@@ -0,0 +1,23 @@
+﻿namespace ShootingGame.Entities.Planes.Enemies
+{
+    internal class GoldenEnemy : Enemy
+    {
+        public GoldenEnemy(int x, int y, int level ) : base(x, y, /*r=*/10,  /*speed=*/10, null, /*LV=*/level, /*hp=*/1, /*bulletRadius=*/Bullet.RADIUS_FOR_SMALL, /*maxBulletCooltime=*/200)
+        {
+        }
+
+        public override int GetEXP()
+        {
+            return 100;
+        }
+
+        public override List<Bullet> ShotBullet()
+        {
+            return
+            [
+                new(CenterXForShotBullet, Y, bulletRadius, Speed + 3, 180, 1, Id.ENEMY )
+            ];
+        
+        }
+    }
+}
