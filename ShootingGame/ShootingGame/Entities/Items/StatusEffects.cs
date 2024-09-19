29e57e2ac682570e132e56fa94761ba06d991680
Quu23 <kiyu@kaz.bbiq.jp>
2024-09-19 23:59:59 +0900

    TransientItemクラスに自身の効果の種類を持たせた。効果の種類はStatusEffectsで定義。

---
+++ ShootingGame/ShootingGame/Entities/Items/StatusEffects.cs
@@ -0,0 +1,15 @@
+﻿namespace ShootingGame.Entities.Items
+{
+    /// <summary>
+    /// プレイヤーの状態異常の種類を表すクラス。
+    /// </summary>
+    public enum StatusEffects
+    {
+        NORMALE        =  0,
+        SPEED_UP       =  1,
+        SPEED_DOWN     = -1,
+        SHOT_RATE_UP   =  2,
+        SHOT_RATE_DOWN = -2,
+        INVINCIBLE     =  3,
+    }
+}
--- ShootingGame/ShootingGame/Entities/Items/TransientItem.cs
+++ ShootingGame/ShootingGame/Entities/Items/TransientItem.cs
@@ -13,10 +13,26 @@ namespace ShootingGame.Entities.Items
         /// </summary>
         public readonly int EFFECT_IIME;
 
-        protected TransientItem(int x, int y, int radius, int speed, BitmapImage img, int EFFECT_IIME) : base(x, y, radius, speed, img)
+        private readonly StatusEffects EFFECT_KIND;
+
+        protected TransientItem(int x, int y, int radius, int speed, BitmapImage img, StatusEffects EFFECT_KIND, int EFFECT_IIME) : base(x, y, radius, speed, img)
         {
             this.EFFECT_IIME = EFFECT_IIME;
+            this.EFFECT_KIND = EFFECT_KIND;
         }
+
+        public override sealed void MakeEffect(Player player)
+        {
+            Effect(player);
+            player.status[EFFECT_KIND] += EFFECT_IIME;
+
+        }
+
+        /// <summary>
+        /// 持続効果を表す。例えば、スピードを上げるならSpeed += 1;など。
+        /// </summary>
+        /// <param name="player"></param>
+        protected abstract void Effect(Player player);
 
         /// <summary>
         /// アイテムの効果を切る。効果時間を過ぎたら呼び出す。
