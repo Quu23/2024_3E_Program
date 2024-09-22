65d6c712a6bcd2debf8ced31244c38d3bfae99df
Chato <yukimi1937@icloud.com>
2024-09-22 22:11:51 +0900

    scoreboosteritemを作った

---
+++ ShootingGame/ShootingGame/Entities/Items/ScoreBoosterItem.cs
@@ -0,0 +1,30 @@
+﻿using ShootingGame.Entities.Planes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ShootingGame.Entities.Items
+{
+    class ScoreBoosterItem : TransientItem
+    {
+        public ScoreBoosterItem(int x, int y, int radius, int speed, BitmapImage img, StatusEffects EFFECT_KIND, int EFFECT_IIME) : base(x, y, radius, speed, img, EFFECT_KIND, EFFECT_IIME)
+        {
+        }
+
+       
+        
+
+        protected override void Effect(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
