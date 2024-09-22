b74835f07d4a41080c7a48662fdf92d5c0a0d6e9
Quu23 <kiyu@kaz.bbiq.jp>
2024-09-23 00:07:01 +0900

    DestroyImageの画像を用意。

--- ShootingGame/ShootingGame/Images.cs
+++ ShootingGame/ShootingGame/Images.cs
@@ -16,14 +16,15 @@ namespace ShootingGame
 
 
         //アイテム
-        public static readonly BitmapImage EXP_ORB_IMAGE            = new BitmapImage(UtilityUris.EXP_ORB_URI);
-        public static readonly BitmapImage SHOT_RATE_UP_ITEM_IMAGE  = new BitmapImage(UtilityUris.SHOT_RATE_UP_ITEM_URI);
-        public static readonly BitmapImage SHOT_RATE_DOWN_ITEM_IMAGE= new BitmapImage(UtilityUris.SHOT_RATE_DOWN_ITEM_URI);
-        public static readonly BitmapImage SPEED_UP_ITEM_IMAGE      = new BitmapImage(UtilityUris.SPEED_UP_ITEM_URI);
-        public static readonly BitmapImage SPEED_DOWN_ITEM_IMAGE    = new BitmapImage(UtilityUris.SPEED_DOWN_ITEM_URI);
-        public static readonly BitmapImage HEALING_ITEM_IMAGE       = new BitmapImage(UtilityUris.HEALIING_ITEM_URI);
-        public static readonly BitmapImage INVINCIBLE_ITEM_IMAGE    = new BitmapImage(UtilityUris.INVINCIBLE_ITEM_URI);
-        public static readonly BitmapImage CLEAR_ENEMIES_ITEM_IMAGE = new BitmapImage(UtilityUris.CLEAR_ENEMIES_ITEM_URI);
+        public static readonly BitmapImage EXP_ORB_IMAGE             = new BitmapImage(UtilityUris.EXP_ORB_URI);
+        public static readonly BitmapImage SHOT_RATE_UP_ITEM_IMAGE   = new BitmapImage(UtilityUris.SHOT_RATE_UP_ITEM_URI);
+        public static readonly BitmapImage SHOT_RATE_DOWN_ITEM_IMAGE = new BitmapImage(UtilityUris.SHOT_RATE_DOWN_ITEM_URI);
+        public static readonly BitmapImage SPEED_UP_ITEM_IMAGE       = new BitmapImage(UtilityUris.SPEED_UP_ITEM_URI);
+        public static readonly BitmapImage SPEED_DOWN_ITEM_IMAGE     = new BitmapImage(UtilityUris.SPEED_DOWN_ITEM_URI);
+        public static readonly BitmapImage HEALING_ITEM_IMAGE        = new BitmapImage(UtilityUris.HEALIING_ITEM_URI);
+        public static readonly BitmapImage INVINCIBLE_ITEM_IMAGE     = new BitmapImage(UtilityUris.INVINCIBLE_ITEM_URI);
+        public static readonly BitmapImage CLEAR_ENEMIES_ITEM_IMAGE  = new BitmapImage(UtilityUris.CLEAR_ENEMIES_ITEM_URI);
+        public static readonly BitmapImage DESTROY_ITEM_IMAGE        = new BitmapImage(UtilityUris.GOLDEN_ENEMY_URI);
 
         //プレイヤー
         public static readonly BitmapImage PLAYER_IMAGE             = new BitmapImage(UtilityUris.PLAYER_URI);
