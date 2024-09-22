3793deda29f3fc7da383a67638b7f54ff7e7a946
niwatori1gou <niwatori22031@icloud.com>
2024-09-22 16:11:01 +0900

    新しい敵を追加した。

--- ShootingGame/ShootingGame/Entities/Planes/Enemies/SplashEnemy.cs
+++ ShootingGame/ShootingGame/Entities/Planes/Enemies/SplashEnemy.cs
@@ -12,7 +12,7 @@ namespace ShootingGame.Entities.Planes.Enemies
         private int degree;
         int a = 1;
 
-        public SplashEnemy(int x, int y,  int level) : base(x, y, 20, 3, Images.STRAIGHT_ENEMY_IMAGE, level, 5 ,Bullet.RADIUS_FOR_SMALL ,10)
+        public SplashEnemy(int x, int y,  int level) : base(x, y, 20, 3, Images.STRAIGHT_ENEMY_IMAGE, level, 5 ,Bullet.RADIUS_FOR_SMALL ,9)
         {
             this.degree = 150;
         }
@@ -27,7 +27,7 @@ namespace ShootingGame.Entities.Planes.Enemies
             var bullets = new List<Bullet>() {
                 new Bullet(CenterXForShotBullet,Y,bulletRadius,Speed+5,degree,3,Id.ENEMY),
             };
-            if (this.degree <= 150) {a = 1; }
+            if (this.degree <= 150){a = 1; }
             else if(this.degree >= 210){a = -1; }
 
             degree += 10 * a;
