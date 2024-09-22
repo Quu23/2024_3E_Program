f22edbce51b1cd85c192f5a8097619ad6590991b
xenon <tmhajime0905@gmai.com>
2024-09-22 23:05:55 +0900

    すべての敵の調整

--- ShootingGame/ShootingGame/Entities/Planes/Enemies/SnakeEnemy.cs
+++ ShootingGame/ShootingGame/Entities/Planes/Enemies/SnakeEnemy.cs
@@ -2,7 +2,7 @@
 {
     class SnakeEnemy : Enemy
     {
-        public SnakeEnemy(int x, int y, int level) : base(x, y, /*r=*/20, /*speed=*/5, Images.SNAKE_ENEMY_IMAGE, /*LV=*/level, /*hp=*/3, /*bulletRadius=*/Bullet.RADIUS_FOR_MEDIUM, 50)
+        public SnakeEnemy(int x, int y, int level) : base(x, y, /*r=*/20, /*speed=*/5, Images.SNAKE_ENEMY_IMAGE, /*LV=*/level, /*hp=*/2 + level, /*bulletRadius=*/Bullet.RADIUS_FOR_MEDIUM, 80)
         {
         }
 
@@ -22,7 +22,7 @@
         protected override void Move()
         {
             base.Move();
-            X = (int)(10 * Speed * Math.Sin(Math.PI / 216 * Y)) + 60;
+            X = (int)(20 * Speed * Math.Sin(Math.PI / 216 * Y)) + 60;
         }
     }
 }
