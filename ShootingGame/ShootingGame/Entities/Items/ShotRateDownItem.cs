d8bec1e5b48586825d83a9530ba5d66985962022
keigo <shixieguiwu@gmail.com>
2024-09-22 17:06:09 +0900

    ShotRateDownItemを追加した

---
+++ ShootingGame/ShootingGame/Entities/Items/ShotRateDownItem.cs
@@ -0,0 +1,21 @@
+﻿using ShootingGame.Entities.Planes;
+
+namespace ShootingGame.Entities.Items
+{
+    class ShotRateDownItem : TransientItem
+    {
+        public ShotRateDownItem(int x, int y) : base(x, y, 8, 4, Images.SHOT_RATE_DOWN_ITEM_IMAGE, StatusEffects.SHOT_RATE_DOWN, 100)
+        {
+        }
+
+        protected override void Effect(Player player)
+        {
+            player.DecreaceBulletCoolTime /= 3;
+        }
+
+        protected override void Move()
+        {
+            Y += Speed;
+        }
+    }
+}
