c2b817912cac4518c426fe21d61cef9cb32e3275
Quu23 <kiyu@kaz.bbiq.jp>
2024-10-22 00:05:34 +0900

    BOSSの土台は作った。

---
+++ ShootingGame/ShootingGame/Entities/Planes/Enemies/Boss/Boss.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Windows.Media.Imaging;
+
+namespace ShootingGame.Entities.Planes.Enemies.Boss
+{
+    abstract class Boss : Enemy
+    {
+
+        protected List<Enemy> followers;
+
+        public Boss(int x, int y, int radius, int speed, BitmapImage img, int level, int hp, int bulletRadius, int maxBulletCoolTime, List<Enemy> followers) : base(x, y, radius, speed, img, level, hp, bulletRadius, maxBulletCoolTime)
+        {
+            this.followers = followers;
+            foreach (Enemy enemy in followers)
+            {
+                hp += enemy.Hp;
+            }
+        }
+
+        public override void Action()
+        {
+            base.Action();
+
+            foreach (Enemy enemy in followers)
+            {
+                enemy.Action();
+            }
+        }
+    }
+}
